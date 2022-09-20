af8f5647ad324cd78bd4f1d53be01bcf45bd5727
astaneve <asta.ilgaude@gmail.com>
2022-09-20 19:27:30 +0300

    Add NODE_js exam files

---
+++ src/index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const cors = require("cors");
+const mysql = require("mysql2/promise");
+
+const app = express(); 
+app.use(express.json()); 
+app.use(cors()); 
+
+const { port } = require("./config");
+
+const users = require("./routes/users");
+
+app.use("/users/", users);
+
+app.all("*", (req, res) => {
+  res.status(404).send({ error: "Page not found" });
+});
+
+app.listen(port, () => 
+console.log(`Listening on port ${port}`));
+
+app.get("/", async (req, res) => {
+    try {
+      const con = await mysql.createConnection(dbconfig);
+      res.send("Success");
+      await con.end();
+    } catch (e) {
+      console.log(e);
+    }
+  });
